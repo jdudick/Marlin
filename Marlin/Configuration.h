f2ab0a63afe0e44993583b4a5c267994a480b6e5
Jeremy Dudick <jeremy.dudick@gmail.com>
2020-07-28 19:04:16 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -854,7 +854,7 @@
 //#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
-//#define USE_PROBE_FOR_Z_HOMING
+#define USE_PROBE_FOR_Z_HOMING
 
 /**
  * Z_MIN_PROBE_PIN
@@ -1123,12 +1123,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 325
+#define X_BED_SIZE 327
 #define Y_BED_SIZE 310
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -16
-#define Y_MIN_POS -3
+#define X_MIN_POS -15
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
