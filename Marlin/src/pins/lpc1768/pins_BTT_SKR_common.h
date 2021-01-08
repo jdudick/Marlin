d1a491a53cac2fc67638e10247487f74791dd0d0
Jeremy Dudick <jeremy.dudick@gmail.com>
2021-01-08 10:01:43 -0500

    Updates

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_common.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_common.h
@@ -83,7 +83,7 @@
   #endif
 #endif
 #ifndef FAN_PIN
-  #define FAN_PIN                          P2_03
+  #define FAN_PIN                          0
 #endif
 #ifndef HEATER_BED_PIN
   #define HEATER_BED_PIN                   P2_05
